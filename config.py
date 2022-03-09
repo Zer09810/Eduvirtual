5fc5251d38dbce2babb44dadd35b5b6e39950a1e
Zer09810 <100539813+Zer09810@users.noreply.github.com>
2022-03-09 14:43:45 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
-BOT_TOKEN = '' #token de tu bot
-API_ID = 7684605 # lo creas en https://my.telegram.org/
-API_HASH = ''  # lo creas en https://my.telegram.org/
-OWNER = [5004180573]
+BOT_TOKEN = '5288013252:AAHIHy4H_Gqf523MI2pFDaQf71onzYT4nAg' #token de tu bot
+API_ID = 13612417 # lo creas en https://my.telegram.org/
+API_HASH = 'e9eea522c02b99992d71941c893d4b5c'  # lo creas en https://my.telegram.org/
+OWNER = [1213738480]
 CHAT_ID = '-1001666279733'
 
 # DATOS DEL MOODLE
-USUARIO = ''
-MOODLE_PASSWORD = ''
-USUARIO_ID = 32764 #Tu id
+USUARIO = 'yeima'
+MOODLE_PASSWORD = 'yeima0428'
+USUARIO_ID = 20005 #Tu id
 ZIP_MB = 249
-MOODLE_URL = 'https://evea.uh.cu'
+MOODLE_URL = 'https://eduvirtual.uho.edu.cu'
 
 # CUENTA DE MEGA - este bot sirve par mega tambien
 GMAIL_MEGA = 'AQUI TU GMAIL' 
